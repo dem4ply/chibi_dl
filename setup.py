19834cc023d2fa8637749c2489e14233cba658af
dem4ply <dem4ply@gmail.com>
2024-08-05 01:06:44 -0600

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join( here, 'README.rst' ) ).read()
 
 setup(
     name='chibi_dl',
-    version='0.1.2',
+    version='0.2.0',
     description='',
     long_description=README,
     license='',
